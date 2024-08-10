e8a30158904a41162ae9c2d68497a83503c363b9
polyakszabolcs92 <149954190+polyakszabolcs92@users.noreply.github.com>
2024-08-10 12:38:22 +0000

    v0.1

--- functions_material.py
+++ functions_material.py
@@ -1,21 +1,4 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-
-# IMPORT MATERIAL PROPERTIES FROM CSV FILES
-@st.cache_data
-def csv_import():
-    df_concrete = pd.read_csv("static/concrete_data.csv", delimiter=";", header=0, index_col=0)
-    df_rebar = pd.read_csv("static/rebar_data.csv", delimiter=";", header=0, index_col=0)
-    df_strand = pd.read_csv("static/strand_data.csv", delimiter=";", header=0, index_col=0)
-    return df_concrete, df_rebar, df_strand
-
-df_concrete, df_rebar, df_strand = csv_import()
-
-# LIST OF STRENGTH CLASSES OF MATERIALS FOR DROPDOWN LISTS
-concrete_classes = df_concrete.index.values.tolist()
-rebar_classes = df_rebar.index.values.tolist()
-strand_classes = df_strand.index.values.tolist()
 
 # 'Lambda' factor for effective height of compressive zone
 def cal_lambda(fck):
@@ -38,42 +21,41 @@ def cal_Ecm(fcm):
     E = 22*(fcm/10)**(0.3) *1000
     return E
 
-
 # GET CONCRETE PROPERTIES AS A LIST
-def concrete_props(concrete, alpha, pfactor):
-    fck = df_concrete.loc[concrete, 'fck']          
+def concrete_props(concrete, alpha, pfactor, df):
+    fck = df.loc[concrete, 'fck']          
     eta = cal_eta(fck)                               
     fcd = alpha * eta * fck / pfactor
-    eps_cu = df_concrete.loc[concrete, 'eps_cu']
+    eps_cu = df.loc[concrete, 'eps_cu']
     lambda_x = cal_lambda(fck)
-    fctm = df_concrete.loc[concrete, 'fctm']                   
-    fcm = df_concrete.loc[concrete, 'fcm']
+    fctm = df.loc[concrete, 'fctm']                   
+    fcm = df.loc[concrete, 'fcm']
     Ecm = cal_Ecm(fcm)
 
-    return fck, fcd, eps_cu, lambda_x, fctm, Ecm
+    return fck, fcd, eps_cu, lambda_x, fctm, Ecm, fcm
 
 # GET REBAR PROPERTIES AS A LIST
-def rebar_props(rebar, pfactor):
-    fyk = df_rebar.loc[rebar, 'fyk']
-    eps_usk = df_rebar.loc[rebar, 'eps.us']
+def rebar_props(rebar, pfactor, df):
+    fyk = df.loc[rebar, 'fyk']
+    eps_usk = df.loc[rebar, 'eps.us']
     eps_usd = 0.9*eps_usk
-    Es = df_rebar.loc[rebar, 'Es']
+    Es = df.loc[rebar, 'Es']
     fyd = fyk / pfactor
-    k = df_rebar.loc[rebar, 'k']
+    k = df.loc[rebar, 'k']
     ftd = k*fyd                                     
     eps_sy = fyd / Es
 
     return fyk, fyd, eps_usd, eps_sy, Es, ftd
 
 # GET STRAND PROPERTIES AS A LIST
-def strand_props(strand, pfactor):
-    dnom_p = df_strand.loc[strand, 'd.nom']
-    fp_01k = df_strand.loc[strand, 'fp.0.1k']
-    fpk = df_strand.loc[strand, 'fpk']
-    Ap_i = df_strand.loc[strand, 'Ap']
-    eps_upk = df_strand.loc[strand, 'eps.upk']
+def strand_props(strand, pfactor, df):
+    dnom_p = df.loc[strand, 'd.nom']
+    fp_01k = df.loc[strand, 'fp.0.1k']
+    fpk = df.loc[strand, 'fpk']
+    Ap_i = df.loc[strand, 'Ap']
+    eps_upk = df.loc[strand, 'eps.upk']
     eps_upd = 0.9*eps_upk
-    Ep = df_strand.loc[strand, 'Ep']
+    Ep = df.loc[strand, 'Ep']
     fpd = fp_01k / pfactor
     fp_ud = fpd / 0.9
     eps_py = fpd / Ep
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,28 @@
 import streamlit as st
-
+import pandas as pd
+import numpy as np
 import functions_material as rc
 
-# PROJECT DATA INPUT
+# PAGE CONFIG
+st.set_page_config(page_title="Spannbett-psz", layout="centered")
+
+# IMPORT MATERIAL PROPERTIES FROM CSV FILES
+@st.cache_data
+def csv_import():
+    df_concrete = pd.read_csv("static/concrete_data.csv", delimiter=";", header=0, index_col=0)
+    df_rebar = pd.read_csv("static/rebar_data.csv", delimiter=";", header=0, index_col=0)
+    df_strand = pd.read_csv("static/strand_data.csv", delimiter=";", header=0, index_col=0)
+    return df_concrete, df_rebar, df_strand
+
+df_concrete, df_rebar, df_strand = csv_import()
+
+# LIST OF STRENGTH CLASSES OF MATERIALS FOR DROPDOWN LISTS
+concrete_classes = df_concrete.index.values.tolist()
+rebar_classes = df_rebar.index.values.tolist()
+strand_classes = df_strand.index.values.tolist()
+
+
+# PROJECT DATA INPUT-----------------------------------------------------------------
 with st.popover("Project data"):
     project = st.text_input("Project name", placeholder="Type project name here")
     element = st.text_input("Element ID", placeholder="Type element ID here")
@@ -23,10 +43,10 @@ with tab1:
 
     ccol1, ccol2 = st.columns(2)
     with ccol1:
-        concrete = st.selectbox("Concrete strength class ($t_{inf}$)",
-                                options=rc.concrete_classes,
+        concrete = st.selectbox("Concrete strength class",
+                                options=concrete_classes,
                                 index=5)
-        alpha_cc = st.number_input(r"$\alpha_{cc}$",
+        alpha_cc = st.number_input(r"Factor for long-term effects ($\alpha_{cc}$) ",
                                    value= 1.00,
                                    format="%0.2f",
                                    step=0.05,
@@ -42,19 +62,19 @@ with tab1:
                                     step=0.05)
         
     with ccol2:
-        fck = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[0]
-        fcd = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[1]
-        eps_cu = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[2]
-        lambda_x = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[3]
-        fctm = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[4]
-        Ecm = rc.concrete_props(concrete, alpha_cc, pfactor_rc)[5]
+        fck = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[0]
+        fcd = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[1]
+        eps_cu = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[2]
+        lambda_x = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[3]
+        fctm = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[4]
+        Ecm = rc.concrete_props(concrete, alpha_cc, pfactor_rc, df_concrete)[5]
 
         st.markdown("$f_{ck}$"+" = {} N/mm$^2$ (comp. strength, char. value)".format(round(fck, 1)))
         st.markdown("$f_{cd}$"+" = {} N/mm$^2$ (comp. strength, design value)".format(round(fcd, 1)))
         st.markdown(r"$\epsilon_{cu}$"+" = {} (limit compressive strain)".format(round(eps_cu, 4)))
         st.markdown(r"$\lambda$"+" = {}  (factor for height of comp. zone)".format(round(lambda_x, 2)))
         st.markdown("$f_{ctm}$"+" = {} N/mm$^2$ (mean tensile strength)".format(round(fctm, 2)))
-        st.markdown("$E_{cm}$"+" = {} N/mm$^2$ (Young modulus, mean value)".format(round(Ecm, 0)))
+        st.markdown("$E_{cm}$"+" = {} N/mm$^2$ (Modulus of elasticity, mean value)".format(round(Ecm, 0)))
 
     # REBAR
     st.divider()
@@ -63,7 +83,7 @@ with tab1:
     
     with scol1:
         rebar = st.selectbox("Steel strength class",
-                             options=rc.rebar_classes,
+                             options=rebar_classes,
                              index=1)
         pfactor_rebar = st.number_input(r"Partial factor - steel ($\gamma_{s}$)", 
                                         value=1.15,
@@ -71,12 +91,12 @@ with tab1:
                                         step=0.05)
     
     with scol2:
-        fyk = rc.rebar_props(rebar, pfactor_rebar)[0]
-        fyd = rc.rebar_props(rebar, pfactor_rebar)[1]
-        eps_usd = rc.rebar_props(rebar, pfactor_rebar)[2]
-        eps_sy = rc.rebar_props(rebar, pfactor_rebar)[3]
-        Es = rc.rebar_props(rebar, pfactor_rebar)[4]
-        ftd = rc.rebar_props(rebar, pfactor_rebar)[5]
+        fyk = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[0]
+        fyd = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[1]
+        eps_usd = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[2]
+        eps_sy = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[3]
+        Es = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[4]
+        ftd = rc.rebar_props(rebar, pfactor_rebar, df_rebar)[5]
 
         # st.markdown("Rebar properties")
         st.markdown("$f_{yk}$"+" = {} N/mm$^2$ (steel strength, char. value)".format(round(fyk, 1)))
@@ -96,7 +116,7 @@ with tab1:
     
     with pcol1:
         strand = st.selectbox("Strand strength class",
-                             options=rc.strand_classes,
+                             options=strand_classes,
                              index=1)
         pfactor_strand = st.number_input(r"Partial factor - strand ($\gamma_{p}$)", 
                                         value=1.15,
@@ -104,14 +124,14 @@ with tab1:
                                         step=0.05)
     
     with pcol2:
-        dnom_p = rc.strand_props(strand, pfactor_strand)[0]
-        Ap_i = rc.strand_props(strand, pfactor_strand)[1]
-        fp_01k = rc.strand_props(strand, pfactor_strand)[2]
-        fpd = rc.strand_props(strand, pfactor_strand)[3]
-        fp_ud = rc.strand_props(strand, pfactor_strand)[4]
-        eps_upd = rc.strand_props(strand, pfactor_strand)[5]
-        eps_py = rc.strand_props(strand, pfactor_strand)[6]
-        Ep = rc.strand_props(strand, pfactor_strand)[7]
+        dnom_p = rc.strand_props(strand, pfactor_strand, df_strand)[0]
+        Ap_i = rc.strand_props(strand, pfactor_strand, df_strand)[1]
+        fp_01k = rc.strand_props(strand, pfactor_strand, df_strand)[2]
+        fpd = rc.strand_props(strand, pfactor_strand, df_strand)[3]
+        fp_ud = rc.strand_props(strand, pfactor_strand, df_strand)[4]
+        eps_upd = rc.strand_props(strand, pfactor_strand, df_strand)[5]
+        eps_py = rc.strand_props(strand, pfactor_strand, df_strand)[6]
+        Ep = rc.strand_props(strand, pfactor_strand, df_strand)[7]
 
         # st.markdown("Rebar properties")
         st.markdown("$d_{nom}$"+" = {} mm (nominal diameter)".format(round(dnom_p, 1)))
@@ -122,7 +142,6 @@ with tab1:
         st.markdown(r"$\epsilon_{up,d}$"+" = {} (limit strain)".format(round(eps_upd, 4)))
         st.markdown(r"$\epsilon_{py}$"+" = {} (yield strain)".format(round(eps_py, 5)))
         st.markdown("$E_{p}$"+" = {} N/mm$^2$ (Young modulus)".format(round(Ep, 0)))
-
 
 
 # GEOMETRY INPUT---------------------------------------------------------------------------
@@ -137,10 +156,14 @@ with tab2:
         bw = st.number_input("Web width (bw) [cm]", min_value=0., value= 16.0, format="%0.1f")
         b_top = 0
         t_t1 = 0
-        t_t2 = 3
+        t_t2 = 0
         b_bot = 0
         t_b1 = 0
         t_b2 = 0
+        b_top_trapz = 0
+        h_end = h
+        bw_end = bw
+        h_sub = h
 
         if CS_shape == "T":
             b_top = st.number_input("Top flange width (b.top) [cm]", min_value=0., value= 50.0, format="%0.1f")
@@ -164,12 +187,88 @@ with tab2:
             t_b1 = st.number_input("Bottom flange height (t.b1) [cm]", min_value=0., value= 25.0, format="%0.1f")
 
 
-
     with gcol2:
         L = st.number_input("Beam length [m]", 
                             min_value=0.,
                             value= 12.00,
                             format="%0.2f")
-        var_height = st.radio("Variable height", options=["Yes", "No"])
+        var_height = st.checkbox("Variable height", value=False)
         
+        if var_height:
+            slope = st.number_input("Slope [%]", min_value=0.,
+                                    value=3.0,
+                                    format="%0.1f")
+            h_end = h - (L*100/2)*(slope/100)
+            k_height = 0.85      # interpolation factor between beam end and center
+            h_sub = k_height*h + (1-k_height)*h_end
+                
+        if CS_shape == "I":
+            web_widening = st.checkbox("Widened web at beam end", value=False)
+            if web_widening:
+                bw_end = st.number_input("Web width at beam end", min_value=0.,
+                                        value=b_bot,
+                                        format="%0.1f")
+        
+    # Gathering cross-section dimensions in a list and converting to [mm]
+    cs_dims = [x*10 for x in [h, bw, b_top, t_t1, t_t2, b_bot, t_b1, t_b2, b_top_trapz, h_end]]
+
+with tab3:
+    rcol1, rcol2 = st.columns(2)
+    with rcol1:
+        st.markdown("Tensile reinforcement (bottom)")
+        df_As_tens = st.data_editor(pd.DataFrame(data= np.array([[20, 2, 45]]),
+                                                columns=["d [mm]", "pcs", "zi [mm]"],
+                                                index=[0]),
+                                    num_rows='dynamic')
+        data_As_tens = df_As_tens.to_numpy()
+
+    with rcol2:
+        st.markdown("Compressive reinforcement (top)")
+        df_As_comp = st.data_editor(pd.DataFrame(data= np.array([[16, 2, 45]]),
+                                                columns=["d [mm]", "pcs", "zi [mm]"],
+                                                index=[0]),
+                                    num_rows='dynamic')
+        data_As_comp = df_As_comp.to_numpy()
     
+
+    st.divider()
+    st.markdown("Prestressing")
+    pcol1, pcol2, pcol3 = st.columns(3)
+    with pcol1:
+        df_Ap = st.data_editor(pd.DataFrame(data= np.array([[100, 3],
+                                                            [140, 0],
+                                                            [180, 0],
+                                                            [220, 0],
+                                                            [260, 0],
+                                                            [300, 0],
+                                                            [340, 0],
+                                                            [380, 0]]),
+                                                columns=["zi [mm]", "pcs"],
+                                                index=np.arange(0,8,1)),
+                                    num_rows='fixed',
+                                    hide_index=True)
+        data_Ap = df_Ap.to_numpy()
+    
+    with pcol2:
+        sig_p0 = st.number_input(r"Prestress (N/mm$^2$)", value=1000, min_value=0, step=50)
+        kp_red = st.number_input("Reduction factor for estimated prestress losses", value=0.85, min_value=0., step=0.05)
+        concrete_t0 = st.selectbox("Concrete strength at prestress (t0)",
+                                    options=concrete_classes,
+                                    index=2)
+        cement = st.selectbox("Cement type", options= ['R', 'N'], index=0)
+        
+    with pcol3:
+        t0 = st.number_input(r"Time of prestressing ($t_{0}$)", value=3, min_value=0, step=1)
+        t1 = st.number_input(r"Time of load application ($t_{1}$)", value=28, min_value=7, step=1)
+        t_inf = st.number_input(r"Design working life ($t_{inf}$)", value=18250, min_value=100, step=50)
+        RH_t0 = st.number_input("Humidity in temporary state [%]", value=80, min_value=0, step=5)
+        RH_inf = st.number_input("Humidity in final state [%]", value=50, min_value=0, step=5)
+
+# Concrete properties in temporary conditions
+fck_t0 = rc.concrete_props(concrete_t0, alpha_cc, pfactor_rc, df_concrete)[0]
+fctm_t0 = rc.concrete_props(concrete_t0, alpha_cc, pfactor_rc, df_concrete)[4]
+fcm_t0 = rc.concrete_props(concrete_t0, alpha_cc, pfactor_rc, df_concrete)[6]
+Ecm_t0 = rc.concrete_props(concrete_t0, alpha_cc, pfactor_rc, df_concrete)[5]
+
+
+# LOADS INPUT---------------------------------------------------------------------------
